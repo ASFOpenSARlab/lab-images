1c561503c0543c6fb10df407fb15f2af93387e2a
bbuechle <btbuechler@alaska.edu>
2026-08-18 13:47:53 -0600

    Add GEOS636 - Programming and Automation for Geosciences

---
+++ geos636/hooks/geos636.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -ve
+
+# Add Path to local pip execs.
+export PATH=$HOME/.local/bin:$PATH
+
+# Copy over extension override
+cp /etc/user_server_includes/overrides/geos636.json /opt/conda/share/jupyter/lab/settings/overrides.json
+
+# Disable default Jupyterlab-tours tours
+jupyter labextension disable "jupyterlab-tour:notebook-tours"
+jupyter labextension disable "jupyterlab-tour:default-tours"
+
+# Disable the extension manager in Jupyterlab since server extensions are uninstallable
+# by users and non-server extension installs do not persist over server restarts
+jupyter labextension disable @jupyterlab/extensionmanager-extension
+
+# Disable proxy of virtual desktop with shortcuts. One might be able to get the desktop still via url /desktop.
+jupyter labextension disable @jupyterhub/jupyter-server-proxy
+
+CONDARC=$HOME/.condarc
+if ! test -f "$CONDARC"; then
+	cat <<EOT >>"$CONDARC"
+envs_dirs:
+  - /home/jovyan/.local/envs
+  - /opt/conda/envs
+EOT
+fi
+
+eval "$(mamba shell hook --shell bash)"
